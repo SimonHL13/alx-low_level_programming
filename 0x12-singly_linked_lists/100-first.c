c5763ed3c5ce095ba0d86461b684be8af7e57391
SimonHL13 <usiagwusimon@gmail.com>
2021-07-30 01:49:21 -0700

    function that prints You're beat

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * executeBeforeMain - Prints a string before main
+ * Return: void
+ */
+void executeBeforeMain(void)
+{
+printf("You're beat! and yet, you must allow,\n");
+printf("I bore my house upon my back!\n");
+}
